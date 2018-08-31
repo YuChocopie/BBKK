a0dc80c803da6e858a6c50c993632bc83e4001c9
KimYujeong <rladbwjd341@naver.com>
2018-09-01 01:53:27 +0900

    Add start page move of activity
    StartActivity, TendencyActivity, SeasonActivity, Main Activity 의 페이지 이동기능을 추가한다
    TendencyActivity와 SeasonActivity의 레이아웃은 겹치는 부분이 많기때문에 item_start.xml 을 재활용한다
    
    Fixes https://github.com/dbwjd/BBKK/issues/7

--- app/src/main/java/com/bbkk/android/bbkkclient/SeasonActivity.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/SeasonActivity.java
@@ -1,13 +1,30 @@
 package com.bbkk.android.bbkkclient;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class SeasonActivity extends AppCompatActivity {
+
+  private static final String SEASON_TITLE = "Season";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_season);
+
+    ImageButton nextPageButton = findViewById(R.id.btn_start_next);
+
+    nextPageButton.setOnClickListener(view -> {
+      Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+      startActivity(intent);
+    });
+
+  TextView activityName = findViewById(R.id.textView_start_message);
+  activityName.setText(SEASON_TITLE);
+
   }
 }
--- app/src/main/java/com/bbkk/android/bbkkclient/TendencyActivity.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/TendencyActivity.java
@@ -1,13 +1,28 @@
 package com.bbkk.android.bbkkclient;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class TendencyActivity extends AppCompatActivity {
+
+  private static final String TENDENCY_TITLE = "TENDENCY";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_tendency);
+
+    ImageButton nextPageButton = findViewById(R.id.btn_start_next);
+
+    nextPageButton.setOnClickListener(view -> {
+      Intent intent = new Intent(getApplicationContext(), SeasonActivity.class);
+      startActivity(intent);
+    });
+
+    TextView activityName = findViewById(R.id.textView_start_message);
+    activityName.setText(TENDENCY_TITLE);
   }
 }
