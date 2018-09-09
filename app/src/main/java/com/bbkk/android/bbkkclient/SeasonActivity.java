6db2b0c9986e276a55d0193cff0cf1b121056ca9
KimYujeong <rladbwjd341@naver.com>
2018-09-09 20:32:02 +0900

    Add View Pager of "SeasonActivity"
    - 계절을 나타내는 View에 ViewPager 기능을 추가한다
    
    Fixes https://github.com/dbwjd/BBKK/issues/12

--- app/src/main/java/com/bbkk/android/bbkkclient/SeasonActivity.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/SeasonActivity.java
@@ -1,20 +1,28 @@
 package com.bbkk.android.bbkkclient;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class SeasonActivity extends AppCompatActivity {
 
   private static final String SEASON_TITLE = "Season";
+  int MAX_PAGE=4;
+  Fragment fragment = new Fragment();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_season);
+
+    ViewPager viewPager = findViewById(R.id.season_viewpager);
+    viewPager.setAdapter(new adapter(getSupportFragmentManager()));
 
     ImageButton nextPageButton = findViewById(R.id.btn_start_next);
 
@@ -26,5 +34,38 @@ public class SeasonActivity extends AppCompatActivity {
   TextView activityName = findViewById(R.id.textView_start_message);
   activityName.setText(SEASON_TITLE);
 
+  }
+
+  private class adapter extends FragmentPagerAdapter {
+    public adapter(FragmentManager fm) {
+      super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+      if(position<0 || MAX_PAGE<=position)
+        return null;
+      switch (position){
+        case 0:
+          fragment = new SpringFragment();
+          break;
+        case 1:
+          fragment = new SummerFragment();
+          break;
+        case 2:
+          fragment = new FallFragment();
+          break;
+        case 3:
+          fragment = new WinterFragment();
+
+      }
+
+      return fragment;
+    }
+
+    @Override
+    public int getCount() {
+      return MAX_PAGE;
+    }
   }
 }
