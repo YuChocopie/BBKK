e96a9a0d9a06529e6210d7cdaaaf546f418df9cd
KimYujeong <rladbwjd341@naver.com>
2018-09-11 00:44:40 +0900

    Add global_header
    - 공통으로 쓰일 global header을 만든다
    - FloatingActionButton을 추가한다
    - 상단 상태바를 안보이게한다.
    
    Fixes https://github.com/dbwjd/BBKK/issues/16

--- app/src/main/java/com/bbkk/android/bbkkclient/view/main/MainActivity.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/view/main/MainActivity.java
@@ -1,15 +1,31 @@
 package com.bbkk.android.bbkkclient.view.main;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.bbkk.android.bbkkclient.R;
 
 public class MainActivity extends AppCompatActivity {
-
+  private static final String MAIN_TITLE = "서울의 여름";
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    TextView activityName = findViewById(R.id.textView_global_header);
+    activityName.setText(MAIN_TITLE);
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btn_start_write);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+          .setAction("Action", null).show();
+      }
+    });
   }
 }
