7fcba801fe492d9ff2357329d64a6cafc2a4442e
KimYujeong <rladbwjd341@naver.com>
2018-09-11 23:19:51 +0900

    Reflect Recycler view on 'MainActivity' timeline
    - 메인액티비티 타임라인뷰에 리사이클러뷰를 적용한다.
    - 레이아웃을 수정한다.
    
    Fixes https://github.com/dbwjd/BBKK/issues/18

---
+++ app/src/main/java/com/bbkk/android/bbkkclient/presenter/main/TimeLineViewHolder.java
@@ -0,0 +1,22 @@
+package com.bbkk.android.bbkkclient.presenter.main;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bbkk.android.bbkkclient.R;
+
+public class TimeLineViewHolder extends RecyclerView.ViewHolder {
+
+  TextView timeLineName;
+  ImageView timeLineimage;
+
+
+  TimeLineViewHolder (View itemView) {
+    super(itemView);
+    timeLineName = itemView.findViewById(R.id.textView_timeline_name);
+    timeLineimage = itemView.findViewById(R.id.imageView_timeline_honey_image);
+  }
+
+}
--- app/src/main/java/com/bbkk/android/bbkkclient/view/main/MainActivity.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/view/main/MainActivity.java
@@ -4,13 +4,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.bbkk.android.bbkkclient.R;
+import com.bbkk.android.bbkkclient.model.Timeline;
+import com.bbkk.android.bbkkclient.presenter.main.TimeLineAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+  RecyclerView mRecyclerView;
   private static final String MAIN_TITLE = "서울의 여름";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -22,5 +30,21 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab = findViewById(R.id.btn_start_write);
     fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
       .setAction("Action", null).show());
+
+    mRecyclerView = findViewById(R.id.timeline_recycler_view);
+    mRecyclerView.setHasFixedSize(true);
+    LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+    layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+    mRecyclerView.setLayoutManager(layoutManager);
+    ArrayList<Timeline> timelineArrayList = new ArrayList<>();
+
+    timelineArrayList.add(new Timeline(R.drawable.choice,"남한산성"));
+    timelineArrayList.add(new Timeline(R.drawable.choice,"백두산"));
+    timelineArrayList.add(new Timeline(R.drawable.choice,"가을음악회"));
+    timelineArrayList.add(new Timeline(R.drawable.choice,"유정이와함께 한강 자전거"));
+
+    TimeLineAdapter timeLineAdapter = new TimeLineAdapter(timelineArrayList);
+
+    mRecyclerView.setAdapter(timeLineAdapter);
   }
 }
