1a6b78a9fb1fd8991e2bced3b3dce676dc85829c
KimYujeong <rladbwjd341@naver.com>
2018-09-20 08:18:34 +0900

    feat: Add MVP pattern to `Main Activity`
    - `MainActivity`에 MVP패턴을 적용한다.
    
    Fixes https://github.com/dbwjd/BBKK/issues/25

--- app/src/main/java/com/bbkk/android/bbkkclient/view/main/MainActivity.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/view/main/MainActivity.java
@@ -10,41 +10,38 @@ import android.widget.TextView;
 
 import com.bbkk.android.bbkkclient.R;
 import com.bbkk.android.bbkkclient.model.Timeline;
+import com.bbkk.android.bbkkclient.presenter.MainPresenter;
 import com.bbkk.android.bbkkclient.presenter.TimeLineAdapter;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainContract.View{
 
-  RecyclerView mRecyclerView;
+  private RecyclerView mRecyclerView;
   private static final String MAIN_TITLE = "서울의 여름";
+  private MainContract.Presenter presenter;
+  private TextView activityName;
+  private FloatingActionButton fabStartWrite;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    presenter();
+  }
 
-    TextView activityName = findViewById(R.id.textView_global_header);
-    activityName.setText(MAIN_TITLE);
-
-    FloatingActionButton fab = findViewById(R.id.btn_start_write);
-    fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-      .setAction("Action", null).show());
-
+  private void presenter() {
     mRecyclerView = findViewById(R.id.timeline_recycler_view);
-    mRecyclerView.setHasFixedSize(true);
-    LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-    layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-    mRecyclerView.setLayoutManager(layoutManager);
-    ArrayList<Timeline> timelineArrayList = new ArrayList<>();
+    presenter = new MainPresenter(this);
+    presenter.callRecyclerView(mRecyclerView);
+  }
 
-    timelineArrayList.add(new Timeline(R.drawable.choice,"남한산성"));
-    timelineArrayList.add(new Timeline(R.drawable.choice,"백두산"));
-    timelineArrayList.add(new Timeline(R.drawable.choice,"가을음악회"));
-    timelineArrayList.add(new Timeline(R.drawable.choice,"유정이와함께 한강 자전거"));
-
-    TimeLineAdapter timeLineAdapter = new TimeLineAdapter(timelineArrayList);
-
-    mRecyclerView.setAdapter(timeLineAdapter);
+  @Override
+  public void initView() {
+    activityName = findViewById(R.id.textView_global_header);
+    fabStartWrite = findViewById(R.id.btn_start_write);
+    activityName.setText(MAIN_TITLE);
+    fabStartWrite.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+      .setAction("Action", null).show());
   }
 }
