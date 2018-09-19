e683ea0f58b1afbe5f5601f9c9da11ca2504b604
KimYujeong <rladbwjd341@naver.com>
2018-09-20 06:22:14 +0900

    feat: Add MVP pattern to `Tendency Activity'
    TendencyActivity에 MVP패턴 적용한다.
    뷰플리퍼를 제거하고 뷰페이저 적용한다.
    
    Fixes https://github.com/dbwjd/BBKK/issues/23

--- app/src/main/java/com/bbkk/android/bbkkclient/view/tendency/TendencyActivity.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/view/tendency/TendencyActivity.java
@@ -1,97 +1,65 @@
 package com.bbkk.android.bbkkclient.view.tendency;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.ViewFlipper;
-
 import com.bbkk.android.bbkkclient.R;
-import com.bbkk.android.bbkkclient.ViewFlipperAction;
+import com.bbkk.android.bbkkclient.presenter.TendencyPresenter;
 import com.bbkk.android.bbkkclient.view.season.SeasonActivity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class TendencyActivity extends AppCompatActivity implements ViewFlipperAction.ViewFlipperCallback{
+public class TendencyActivity extends AppCompatActivity implements TendencyContract.View {
 
   private static final String TENDENCY_TITLE = "TENDENCY";
-
-  ViewFlipper flipper;
-  List<ImageView> indexes;
-  private int position;
+  private int MAX_PAGE=5;
+  ViewPager vpTendency;
+  TendencyContract.Presenter presenter;
+  ImageButton ibtnNext;
+  TextView tvTendencyName;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_tendency);
+    presenter();
+  }
 
-    tendencyFlipper();
+  private void presenter() {
+    presenter = new TendencyPresenter(this);
+  }
 
-    ImageButton nextPageButton = findViewById(R.id.btn_start_next);
+  @Override
+  public void initView() {
+    vpTendency = findViewById(R.id.season_viewpager);
+    ibtnNext = findViewById(R.id.btn_start_next);
+    tvTendencyName = findViewById(R.id.textView_start_message);
 
-    nextPageButton.setOnClickListener(view -> {
+    ibtnNext.setOnClickListener(view -> {
       Intent intent = new Intent(getApplicationContext(), SeasonActivity.class);
       startActivity(intent);
     });
 
-    TextView activityName = findViewById(R.id.textView_start_message);
-    activityName.setText(TENDENCY_TITLE);
+    vpTendency.setAdapter(new adapter(getSupportFragmentManager()));
+    tvTendencyName.setText(TENDENCY_TITLE);
   }
 
-  private void tendencyFlipper() {
-    flipper = findViewById(R.id.flipper_tendency);
-    ImageView index0 = findViewById(R.id.imageView_tendency_imgIndex0);
-    ImageView index1 = findViewById(R.id.imageView_tendency_imgIndex1);
-    ImageView index2 = findViewById(R.id.imageView_tendency_imgIndex2);
-    ImageView index3 = findViewById(R.id.imageView_tendency_imgIndex3);
-    ImageView index4 = findViewById(R.id.imageView_tendency_imgIndex4);
-
-    //인덱스리스트
-    indexes = new ArrayList<>();
-    indexes.add(index0);
-    indexes.add(index1);
-    indexes.add(index2);
-    indexes.add(index3);
-    indexes.add(index4);
-
-    //xml을 inflate 하여 flipper view에 추가하기
-    LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-    View view1 = inflater.inflate(R.layout.item_tendency_view_flipper1, flipper, false);
-    View view2 = inflater.inflate(R.layout.item_tendency_view_flipper2, flipper, false);
-    View view3 = inflater.inflate(R.layout.item_tendency_view_flipper3, flipper, false);
-    View view4 = inflater.inflate(R.layout.item_tendency_view_flipper4, flipper, false);
-    View view5 = inflater.inflate(R.layout.item_tendency_view_flipper5, flipper, false);
-    //inflate 한 view 추가
-    flipper.addView(view1);
-    flipper.addView(view2);
-    flipper.addView(view3);
-    flipper.addView(view4);
-    flipper.addView(view5);
-    //리스너설정 - 좌우 터치시 화면넘어가기
-    flipper.setOnTouchListener(new ViewFlipperAction(this, flipper));
-  }
-
-  //인덱스 업데이트
-  @Override
-  public void onFlipperActionCallback(int position) {
-    this.position = position;
-    Log.d("ddd", "" + position);
-    for (int i = 0; i < indexes.size(); i++) {
-      ImageView index = indexes.get(i);
-      //현재화면의 인덱스 위치면 녹색
-      if (i == position) {
-        index.setImageResource(R.drawable.choice);
-      }
-      //그외
-      else {
-        index.setImageResource(R.drawable.not_choice);
-      }
+  private class adapter extends FragmentPagerAdapter {
+    public adapter(FragmentManager fm) {
+      super(fm);
+    }
+    @Override
+    public Fragment getItem(int position) {
+      return presenter.tendencyFragmentGetItem(position,MAX_PAGE);
+    }
+    @Override
+    public int getCount() {
+      return MAX_PAGE;
     }
   }
+
 }
---
+++ app/src/main/java/com/bbkk/android/bbkkclient/view/tendency/TendencyFragment_traveler.java
@@ -0,0 +1,30 @@
+package com.bbkk.android.bbkkclient.view.tendency;
+
+import android.annotation.SuppressLint;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bbkk.android.bbkkclient.R;
+
+public class TendencyFragment_traveler extends android.support.v4.app.Fragment {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+  @SuppressLint("ResourceAsColor")
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    LinearLayout linearLayout=(LinearLayout)inflater.inflate(R.layout.item_tendency_viewpager,container,false);
+    LinearLayout background= linearLayout.findViewById(R.id.fragment_page);
+    TextView page_num= linearLayout.findViewById(R.id.tendency_page_num);
+    page_num.setText("first");
+    background.setBackground(new ColorDrawable(R.color.fall));
+    return linearLayout;
+  }
+}
