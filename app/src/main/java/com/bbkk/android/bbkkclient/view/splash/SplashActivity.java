5cc7e550d95ec66a5ea0a8f1a96a9593dfb864b8
Eastroots92 <eastroots92@gmail.com>
2018-09-23 01:39:18 +0900

    feat: init splashActivity
    splashActivity를 생성한다.

---
+++ app/src/main/java/com/bbkk/android/bbkkclient/view/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.bbkk.android.bbkkclient.view.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bbkk.android.bbkkclient.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash);
+  }
+}
