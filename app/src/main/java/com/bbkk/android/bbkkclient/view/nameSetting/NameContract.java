a47e6fc237e2092f9bfce267e202f07db1d3a710
Eastroots92 <eastroots92@gmail.com>
2018-09-22 18:11:21 +0900

    feat: add changeName function
    버튼 클릭 시 이름을 변경하는 기능을 만든다.

--- app/src/main/java/com/bbkk/android/bbkkclient/view/nameSetting/NameContract.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/view/nameSetting/NameContract.java
@@ -3,13 +3,16 @@ package com.bbkk.android.bbkkclient.view.nameSetting;
 public interface NameContract {
 
   interface View {
-    //닉네임 칸에 뿌려주기
-    void showRandomName(String name);
     void initView();
+
+    void startTendencyActivity();
+
+    void renderChangeName(String currentName);
   }
 
   interface Presenter {
-    //가지고 있는 닉네임 보내 준다 & 새거 받아와
-    void callRandomname(String randomname);
+    void submitAction();
+
+    void changeNameAction();
   }
 }
