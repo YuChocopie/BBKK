529cc203c08ec84910164c9f2e9ee0a4f4ba1d02
Eastroots92 <eastroots92@gmail.com>
2018-09-25 12:56:32 +0900

    feat: add TedPermission Library
    권한을 얻어오는 Permission library를 추가한다.

--- app/src/main/java/com/bbkk/android/bbkkclient/view/write/WriteActivity.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/view/write/WriteActivity.java
@@ -1,13 +1,20 @@
 package com.bbkk.android.bbkkclient.view.write;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bbkk.android.bbkkclient.R;
 import com.bbkk.android.bbkkclient.presenter.WritePresenter;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class WriteActivity extends AppCompatActivity implements WriteContract.View {
   private WriteContract.Presenter presenter;
@@ -20,6 +27,7 @@ public class WriteActivity extends AppCompatActivity implements WriteContract.Vi
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_write);
+    ButterKnife.bind(this);
     presenter = new WritePresenter(this);
   }
 
@@ -27,6 +35,7 @@ public class WriteActivity extends AppCompatActivity implements WriteContract.Vi
   public void initView() {
     this.listenCancel();
     this.listenSubmit();
+    this.setPermission();
   }
 
   private void listenSubmit() {
@@ -39,5 +48,30 @@ public class WriteActivity extends AppCompatActivity implements WriteContract.Vi
     tvCancelBtn.setOnClickListener((__) -> {
       finish();
     });
+  }
+
+  private void setPermission() {
+    PermissionListener permissionlistener = new PermissionListener() {
+      @Override
+      public void onPermissionGranted() {
+        initImagePicker();
+      }
+
+      @Override
+      public void onPermissionDenied(List<String> deniedPermissions) {
+        finish();
+        Toast.makeText(WriteActivity.this, "권한이 없어 내용 등록이 불가능합니다.", Toast.LENGTH_SHORT).show();
+      }
+    };
+
+    new TedPermission().with(getApplicationContext())
+      .setPermissionListener(permissionlistener)
+      .setDeniedMessage("권한 설정 동의를 안하신다면, 나중에 이곳에서 설정해 주세요. [설정] > [권한]")
+      .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+      .check();
+  }
+
+  private void initImagePicker() {
+    
   }
 }
