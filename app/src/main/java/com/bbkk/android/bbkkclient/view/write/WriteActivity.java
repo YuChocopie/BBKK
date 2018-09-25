eb3eae69fcd18cbf6981a8a7a447224125eb4c6d
Eastroots92 <eastroots92@gmail.com>
2018-09-25 13:27:01 +0900

    feat: add imagePicker library
    imagePicker library를 추가한다.

--- app/src/main/java/com/bbkk/android/bbkkclient/view/write/WriteActivity.java
+++ app/src/main/java/com/bbkk/android/bbkkclient/view/write/WriteActivity.java
@@ -1,22 +1,31 @@
 package com.bbkk.android.bbkkclient.view.write;
 
 import android.Manifest;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bbkk.android.bbkkclient.R;
 import com.bbkk.android.bbkkclient.presenter.WritePresenter;
+import com.erikagtierrez.multiple_media_picker.Gallery;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class WriteActivity extends AppCompatActivity implements WriteContract.View {
+  private static final String IMAGE_PICKER_TITLE = "title";
+  private static final String IMAGE_PICKER_VALUE = "이미지 선택";
+  private static final int OPEN_MEDIA_PICKER = 1;
+  private static final String MAX_IMAGE_PICK_TITLE = "maxSelection";
+  private static final int MAX_IMAGE_PICK_VALUE = 5;
   private WriteContract.Presenter presenter;
   @BindView(R.id.tv_header_cancel)
   public TextView tvCancelBtn;
@@ -72,6 +81,28 @@ public class WriteActivity extends AppCompatActivity implements WriteContract.Vi
   }
 
   private void initImagePicker() {
-    
+    Intent intent= new Intent(this, Gallery.class);
+    intent.putExtra(IMAGE_PICKER_TITLE, IMAGE_PICKER_VALUE);
+    intent.putExtra("mode", 2);
+    intent.putExtra(MAX_IMAGE_PICK_TITLE, MAX_IMAGE_PICK_VALUE);
+    startActivityForResult(intent, OPEN_MEDIA_PICKER);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == OPEN_MEDIA_PICKER) {
+      if (resultCode == RESULT_OK && data != null) {
+        ArrayList<String> selectionResult = data.getStringArrayListExtra("result");
+        renderWriteImageView(selectionResult);
+      } else {
+        finish();
+      }
+    }
+  }
+
+  private void renderWriteImageView(ArrayList<String> selectionResult) {
+    ArrayList<String> currentImageResult = selectionResult;
+    Log.e("픽픽", String.valueOf(currentImageResult));
   }
 }
